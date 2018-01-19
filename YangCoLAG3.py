aa32faf46d6c41ff29e866b4358183ba2bee16ac
Paul Feitzinger <paul@pfeyz.com>
2018-01-19 00:47:24 -0500

    add toggle-able relstr

--- YangCoLAG3.py
+++ YangCoLAG3.py
@@ -9,7 +9,7 @@ from __future__ import print_function
 # Yang's Variational Learner (adapted from Sakas 2015)
 # Given:
 # 1)      n, the number of parameters
-# 2)      W=(w1, w2, . . . , wn), a vector of weights, 
+# 2)      W=(w1, w2, . . . , wn), a vector of weights,
 #           where each wi is stipulated to be between 0 and 1
 # 3)      the current grammar (i.e., a vector of parameter values):
 #            Gcurr =(p1, p2,  . . . , pn),
@@ -17,10 +17,10 @@ from __future__ import print_function
 
 ##  Pseudo-code#
 #	For each wi in W, set wi to 0.5
-#	For each randomly chosen input sentence s from the target language: 
+#	For each randomly chosen input sentence s from the target language:
 #	   For each parameter pi in Gcurr :
 #            a. with probability wi , choose the value of pi to be 1;
-#            b. with probability 1 - wi , choose the value of pi to be 0 ; 
+#            b. with probability 1 - wi , choose the value of pi to be 0 ;
 #	Parse s with Gcurr
 #	If s can be parsed by Gcurr, "reward" the weights in W accordingly.
 
@@ -28,7 +28,7 @@ from __future__ import print_function
 ################ Yang Reward-only Learner aligned with code below ###########################
 #
 # 1)    set all elements of Wcurr to .5 (Wcurr is the W in above Pseudo-code)
-# 2)    Gcurr = chooseBasedOn(Wcurr)  -- Choose a grammar randomly weighted by weights in Wcurr vector 
+# 2)    Gcurr = chooseBasedOn(Wcurr)  -- Choose a grammar randomly weighted by weights in Wcurr vector
 # 3)    s = chooseSentUnif() -- Choose a random sentence from the target language
 # 4)    Test if Gcurr can parse (licenses) s
 # 5)    if Gcurr can parse s, reward the system by nudging weights: Wcurr = reward(Wcurr) else do nothing
@@ -39,7 +39,6 @@ from __future__ import print_function
 from collections import defaultdict
 import random
 import datetime
-import csv
 
 ##########
 # Globals - Note "grammars" and "sentences" are globally stored as integer "ids"
@@ -52,7 +51,7 @@ LDsents     = defaultdict(set)
 
 # a list of all valid CoLAG grammar IDs, stored as a dict for efficient lookup
 CoLAG_Gs = {} # Wm note: This should probably be a set of grammar IDs
-                        
+
 
 #################
 ### BEGIN Globals
@@ -62,7 +61,7 @@ n = 13 # number of parameters
 r = .0005  # learning rate
 cr = .0001  # conservative learning rate
 
-trials = 10 # number of simulated learning trials to run
+trials = 100 # number of simulated learning trials to run
 max_sents =  50000 # max number of sents before ending a trial
 threshold = .02 # when all weights are within a threshold, stop
 
@@ -83,6 +82,8 @@ Out_Data_File = Out_Data_Path+Out_Data_File_base+Out_Data_Date_Time_stamp+".csv"
 
 Notes = "" # Prompt user for notes for the current run
 
+USE_REL_STR = True
+DEFAULT_REL_STR = '0' * 13
 
 ##############
 # END Globals
@@ -95,21 +96,21 @@ Notes = "" # Prompt user for notes for the current run
 def setupLD() :
 
   global LDsents, LDstructs, CoLAG_Gs, Notes
- 
+
 
   File = open(LD_File,"r")
   for line in File:
     line = line.rstrip()
-   
+
     [g,       sent,   struct]    = line.split("\t")
     [grammID, sentID, structID, relString] = [int(g), int(sent), int(struct), "~~~~~~~~~~~~~"]
     # when we do relevance we need a way to load proper relevance string, the above is a dummy
-                                                                                                 
+
     # if haven't hit grammID yet in the file, add to list of CoLAG_GS and to LD
     if grammID not in CoLAG_Gs:
         CoLAG_Gs[grammID]=0
         LDsents[grammID]=set()
-    
+
     LDsents  [grammID].add(sentID) # key:int, value:set of ints
 
 
@@ -132,17 +133,17 @@ def reward(relstr):
 def converged():
   global Wcurr
   for i in range(n):
-    if not (1-Wcurr[i] <  threshold  or Wcurr[i] < threshold): 
+    if not (1-Wcurr[i] <  threshold  or Wcurr[i] < threshold):
       return False
   return True
 
 #def converged(): We may add something like below for sub/super and other problem parameters
 #  global Wcurr
 #  for i in range(5):
-#    if not (1-Wcurr[i] <  threshold  or Wcurr[i] < threshold): 
+#    if not (1-Wcurr[i] <  threshold  or Wcurr[i] < threshold):
 #      return False
 #  for i in range(7,n):
-#    if not (1-Wcurr[i] <  threshold  or Wcurr[i] < threshold): 
+#    if not (1-Wcurr[i] <  threshold  or Wcurr[i] < threshold):
 #      return False
 #  return True
 
@@ -163,15 +164,15 @@ def chooseGrammarBasedOn(weights):
 #            Gtmp+="1"
 #        elif i == 3:
 #            Gtmp+="1"
-#        else:  
+#        else:
         x = random.random()
         if x < weights[i]: # checked against Charles code
             Gtmp+="1" # probably more efficient to declare static Gtmp and not append
         else:
             Gtmp+="0"
     return Gtmp
- 
-def chooseSentUnif():  
+
+def chooseSentUnif():
     #UNIFORM
     x = random.randint(0,len(Ltarg)-1)
     sID = Ltarg[x]
@@ -181,13 +182,13 @@ def bin2Dec(bList): # bList is a list of 1's and 0's
     binStr = ""
     for b in bList:
         binStr += str(b)
-    return int(binStr,2)     
- 
+    return int(binStr,2)
+
 def headerOutput(File, NotesP):
     headerstr = "Learning rate:" + str(r) + ", Threshold:" + str(threshold) + ", Note: " + NotesP + ",CovergedID"+","+"\n"
-    File.write(headerstr) 
+    File.write(headerstr)
 
-def csvOutput(File, targ, run, cnt, G, W):
+def csvOutput(File, targ, run, cnt, G, W, time_elapsed):
     Gout =""
     Wout =""
     Pout =""
@@ -196,7 +197,7 @@ def csvOutput(File, targ, run, cnt, G, W):
     for i in range(n):
       Wout += str(round(W[i],15))+","
 
-    outStr = str(targ)+","+str(run)+","+str(cnt)+","+str(bin2Dec(G))+","+"'"+Gout+","+Wout+"\n"
+    outStr = str(targ)+","+str(run)+","+str(cnt)+","+str(bin2Dec(G))+","+"'"+Gout+","+Wout+","+str(time_elapsed)+"\n"
     File.write(outStr)
 
 def readRelevanceStrings(filename):
@@ -230,25 +231,25 @@ def run():
     print("Working ...")
 
     headerOutput(OUTDATA, Notes)
-    
+
     setupLD() # sets up all 3072 potential target grammars/languages
               # into global LD variables
-  
+
     # CoLAG English, Japanese, German and French
     GTARGS =  [ [0,0,0,1,0,0,1,1,0,0,0,1,1] ,  \
                 [0,1,1,1,1,0,0,0,1,0,0,0,0] ,  \
                 [0,1,0,0,0,1,1,0,0,1,1,0,1] ,  \
                 [0,0,0,1,0,0,1,0,0,1,0,0,0]    \
-              ]  
-                
+              ]
+
     GTARGIDS = [611, 3856, 2253, 584]
-    
+
     global Ltarg
 
     relevanceStrDict = readRelevanceStrings('./irrelevance-output.txt')
 
-    for targIdx in range(1): # only Enlgish
-        
+    for targIdx in range(len(GTARGIDS)): # only Enlgish
+
         Gtarg   = GTARGS[targIdx]       # Gtarg is a 13 element list of 0's and 1's
         GtargID = GTARGIDS[targIdx]     # GtargID is a decimal representation of Gtarg
         Ltarg   = list(LDsents[GtargID]) # Ltarg is a list of sentIDs
@@ -262,9 +263,9 @@ def run():
           Wcurr = [.5 for i in range(n)] # initialize weights to 0.5
 
           Gcurr = ""
-          
+
           numSents = 0
-          
+
           CONVERGED = False
 
           while not CONVERGED and numSents < max_sents:
@@ -285,10 +286,13 @@ def run():
 
 
               # if s can be parsed by Gcurr
-              if s in LDsents[GcurrID]: # LD[GcurrID] is a set of sentIDs 
-                  
+              if s in LDsents[GcurrID]: # LD[GcurrID] is a set of sentIDs
+
                   # grab relevance string of s
-                  relevanceStr = relevanceStrDict[s]
+                  if USE_REL_STR:
+                      relevanceStr = relevanceStrDict[s]
+                  else:
+                      relevanceStr = DEFAULT_REL_STR
                   #   picks a parse tree that generates s given Gcurr
                   #   the 'parser' returns a string indicating which parameters were irrelevant
                   #   in terms of building the parse tree
@@ -298,15 +302,17 @@ def run():
                   pass # print("GcurrID: ", GcurrID)
 
 
-                  
 
-              CONVERGED = converged()       
 
-          csvOutput(OUTDATA, GtargID, runNum, numSents, Gcurr , Wcurr)      
+              CONVERGED = converged()
+
 
           end = datetime.datetime.now()
           elapsed = end - start
-          overallElapsed = end - overallStart 
+          overallElapsed = end - overallStart
+
+          csvOutput(OUTDATA, GtargID, runNum, numSents, Gcurr , Wcurr, elapsed)
+
           if runNum % 1 == 0:
             print("GtargID: ", GtargID, "RUN: ", runNum, "took", str(elapsed), "Overall so far: ", overallElapsed)
 
