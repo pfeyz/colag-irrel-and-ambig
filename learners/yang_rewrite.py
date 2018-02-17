30d9fb1add86883adec832c33a141d2106cd159f
Paul Feitzinger <paul@pfeyz.com>
2018-02-17 13:52:46 -0500

    format yang_rewrite output for side-by-side comparison

--- learners/yang_rewrite.py
+++ learners/yang_rewrite.py
@@ -1,4 +1,5 @@
 from colag.colag import Colag, get_param_value, toggled
+from datetime import datetime
 import random
 
 def param_list_to_grammar(params):
@@ -19,6 +20,13 @@ class VariationalLearner:
         self.domain = domain
         self.learning_rate = learning_rate
         self.weights = [0.5] * num_params
+
+    def consume(self, sentence):
+        hypothesis_grammar = self.choose_grammar()
+        if self.parses(hypothesis_grammar, sentence):
+            self.reward(hypothesis_grammar)
+        else:
+            self.punish(hypothesis_grammar)
 
     def converged(self, threshold):
         """Returns true if all values in `weights` list are less than
@@ -46,12 +54,8 @@ class VariationalLearner:
                     grammar = toggled(12 - index, grammar)
         return grammar
 
-    def consume(self, sentence):
-        hypothesis_grammar = self.choose_grammar()
-        if self.parses(hypothesis_grammar, sentence):
-            self.reward(hypothesis_grammar)
-        else:
-            self.punish(hypothesis_grammar)
+    def best_guess(self):
+        return param_list_to_grammar([round(p) for p in self.weights])
 
     def reward(self, hypothesis_grammar):
         for index in range(13):
@@ -61,37 +65,58 @@ class VariationalLearner:
                 self.weights[index] -= self.learning_rate * weight
             elif val == 1:
                 self.weights[index] += self.learning_rate * (1-weight)
-            else:
-                raise Exception()
 
+    def punish(self, hypothesis_grammar):
+        for index in range(13):
+            val = get_param_value(12-index, hypothesis_grammar)
+            weight = self.weights[index]
+            if val == 1:
+                self.weights[index] -= self.learning_rate * weight
+            elif val == 0:
+                self.weights[index] += self.learning_rate * (1-weight)
+
+class RewardOnlyLearner(VariationalLearner):
     def punish(self, hypothesis_grammar):
         pass
 
-class RewardOnlyLearner(VariationalLearner):
-    def punish(self):
+class PunishOnlyLearner(VariationalLearner):
+    def punish(self, hypothesis_grammar):
         pass
 
 def choose_sentence(language):
     return random.choice(language)
 
-def simulate_learning(domain, target_grammar):
+def simulate_learning(learner, target_language, iterations):
     weights = [0.5] * 13
-    target_language = list(domain.language[target_grammar])
-    learner = VariationalLearner(domain, num_params=13)
     threshold = 0.02
     counter = 0
+
     while not learner.converged(threshold):
         sentence = choose_sentence(target_language)
         learner.consume(sentence)
-        if counter >= 50000:
+        if counter >= iterations:
             break
         counter += 1
-    print(counter,
-          param_list_to_grammar([round(x) for x in learner.weights]),
-          round(100 *
-            sum(abs(0.5 - x) * 2 for x in learner.weights) / 13))
+
+    return counter
+
+def timed_apply(fun, *args, **kwargs):
+    start = datetime.now()
+    result = fun(*args, **kwargs)
+    end = datetime.now()
+    return (end - start).total_seconds(), result
+
+def weights_to_params(weights):
+    return ''.join(str(round(x)) for x in weights)
 
 if __name__ == "__main__":
     domain = Colag.default()
-    for i in range(100):
-        simulate_learning(domain, 611)
+    for grammar in [611, 3856, 2253, 584]:
+        language = tuple(domain.language[grammar])
+        for i in range(100):
+            learner = RewardOnlyLearner(domain)
+            runtime, iterations = timed_apply(simulate_learning, learner, language, iterations=50000)
+            gcurr = learner.choose_grammar()
+            col = [grammar, i, iterations, gcurr, '{:013b}'.format(gcurr)] + learner.weights
+            col = col + ['', runtime]
+            print('\t'.join(map(str, col)))
